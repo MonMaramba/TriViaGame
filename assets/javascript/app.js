34774e5c8a85adbc001cf742dee6c2d4ca5b5131
unknown <monmaramba@yahoo.com>
2018-06-18 17:38:23 -0700

    different i logic, game sequence works, plus delay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var qObj1 = {
     ans2: "Timothy Lovejoy",
     ans3: "Apu Nahasapeemapetilon",
     ans4: "Weylon Smithers",
-    ansKey: "Apu Nahasapeemapetilon",
+    ansKey: 3,
 }
 
 var qObj2 = {
@@ -17,23 +17,23 @@ var qObj2 = {
     ans2: "Lurleen Lumpkin",
     ans3: "Marcus 'Marbles' Le Marquez",
     ans4: "Oscar Murphy 'Bleeding Gums' Hibbert",
-    ansKey: "Oscar Murphy 'Bleeding Gums' Hibbert",
+    ansKey: "4",
 }
 var qObj3 = {
-    mainq: "Who Maggie Simpson's nemesis?",
-    ans1: "Gerald Samson",
-    ans2: "Sashi Amelia Nahasapeemapetilon",
-    ans3: "Normal Head Joe Spuckler",
-    ans4: "Stewie Griffin",
-    ansKey: "Gerald Samson",
-}
-var qObj4 = {
     mainq: "Who is Maggie Simpson's nemesis?",
     ans1: "Gerald Samson",
     ans2: "Sashi Amelia Nahasapeemapetilon",
     ans3: "Normal Head Joe Spuckler",
     ans4: "Stewie Griffin",
-    ansKey: "Gerald Samson",
+    ansKey: "1",
+}
+var qObj4 = {
+    mainq: "What part of the opening credits are different every week?",
+    ans1: "Grocery scene with Marge & Maggie",
+    ans2: "Bart riding his skateboard",
+    ans3: "The family on the couch",
+    ans4: "Homer at the nuclear plant",
+    ansKey: "3",
 }
 var qObj5 = {
     mainq: "Although uncredited, who voiced Leon Kompowski's character?",
@@ -41,15 +41,15 @@ var qObj5 = {
     ans2: "Michael Jackson",
     ans3: "Hugh Jackman",
     ans4: "Prince",
-    ansKey: "Michael Jackson",
+    ansKey: "2",
 }
 var qObj6 = {
-    mainq: "Who has the tattoos 'Die Bart, Die', Bart's decapitated head with a speech bubble saying 'Ouch, Man!'?",
+    mainq: "Who has the tattoos 'Die Bart, Die' and Bart's decapitated head with a speech bubble saying 'Ouch, Man!'?",
     ans1: "Comic Book Man",
     ans2: "Sideshow Mel",
     ans3: "Sideshow Bob",
     ans4: "Moe Szyslak",
-    ansKey: "Sideshow Bob",
+    ansKey: "3",
 }
 var qObj7 = {
     mainq: "What is the name of the Comic Book Guy's store",
@@ -57,7 +57,7 @@ var qObj7 = {
     ans2: "Bloodbath & Beyond Gun Shop",
     ans3: "Jeff's Comics Store",
     ans4: "The Android's Dungeon",
-    ansKey: "The Android's Dungeon",
+    ansKey: "4",
 }
 var qObj8 = {
     mainq: "What did Homer smuggle to the space shuttle?",
@@ -65,7 +65,7 @@ var qObj8 = {
     ans2: "Tang Orange Juice",
     ans3: "Potato Chips",
     ans4: "Donuts",
-    ansKey: "Potato Chips",
+    ansKey: "3",
 }
 var qObj9 = {
     mainq: "What is Homer's email address?",
@@ -73,7 +73,7 @@ var qObj9 = {
     ans2: "HomerRocks@thesimpsons.com",
     ans3: "doh@yahoo.com",
     ans4: "donutsrule@gmail.com",
-    ansKey: "ChunkyLover53@aol.com",
+    ansKey: "2",
 }
 var qObj10 = {
     mainq: "What was the title of the Star Trek movie that Bart, Lisa and Grampa watched?",
@@ -81,41 +81,43 @@ var qObj10 = {
     ans2: "Star Trek XIII: A Prequel To A New Beginning",
     ans3: "Star Trek XII: So Very Tired",
     ans4: "Star Trek XV: It Can't Be That Bad",
-    ansKey: "Star Trek XII: So Very Tired",
+    ansKey: "3",
 }
 
 var qObsjArray = [qObj1, qObj2, qObj3, qObj4, qObj5, qObj6, qObj7, qObj8, qObj9, qObj10];
 
 
-//countdown timer function
+//countdown timer function with built-in 0 time condition
 
 var count = 30;
 function cdTimer() {
-
-    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+    
+    var counter = setInterval(timer, 1000); 
 
     function timer() {
         count = count - 1;
         if (count <= 0) {
             wrong++
             clearInterval(counter);
-
-            return;
+            var windowTimeout = setTimeout(function() {
+                nxtChoice();
+              }, 5000);
+              //clearTimeout(windowTimeout);
+                       
         }
         //to display remaining time
         $("#timer").text("Time left: " + count);
     }
 }
 
+
 //choosing question set to display from array
 
+var i = 0;
 
-$("#goBtn").on('click', function () {
-    cdTimer();
-    $("#r2").hide();
 
-    for (i = 0; i < qObsjArray.length; i++) {
-        var mQ = qObsjArray[i].mainq;
+    function qChoice() {
+        
         $("#questionBox").text(qObsjArray[i].mainq);
         $("#option1").text(qObsjArray[i].ans1)
         $("#option2").text(qObsjArray[i].ans2)
@@ -123,23 +125,49 @@ $("#goBtn").on('click', function () {
         $("#option4").text(qObsjArray[i].ans4)
         $("#answerBox").show();
 
-        var counter = 0;
+         
+        console.log(i);
+    };
 
-        (function next() {
-            if (counter++ > maxLoops) return;
+    
 
-            setTimeout(function () {
-                next();
-            }, 1000 * 35);
-        })();
-        console.log(qObsjArray[i].mainq);
+    function nxtChoice() {
+        i = i + 1; 
+        count = 30; 
+        
+        $("#questionBox").text(qObsjArray[i].mainq);
+        $("#option1").text(qObsjArray[i].ans1)
+        $("#option2").text(qObsjArray[i].ans2)
+        $("#option3").text(qObsjArray[i].ans3)
+        $("#option4").text(qObsjArray[i].ans4)
+        $("#answerBox").show();
+
+        cdTimer();
+        
+        /*if (i = 11) {
+            game();
+        }*/
+
+        console.log(i);
     }
+    
+
+$("#goBtn").on('click', function () {
+    cdTimer();
+    $("#r2").hide();
+    qChoice();
+    
 });
 
 
+//listen for user input and compare to answer key
+/*var userChoice 
 
-
-
+    $(".button").on('click', function(){
+      $(".button").val = userChoice;
+      console.log(userChoice);
+    });*/
+  
 
 //create win function
 
